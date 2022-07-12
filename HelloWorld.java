5869d17ad7711e30b9613e0af95b35f19a6d281f
EC2 Default User <ec2-user@ip-172-31-14-167.ap-south-1.compute.internal>
2022-07-12 10:17:59 +0000

    few new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,4 +9,9 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
 }
